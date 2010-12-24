859569a90373b866a3e90a81e2b0eb6c839acb9a
Rafal <rafalx89@gmail.com>
2010-12-24 22:54:19 +0100

    add store controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ActiveRecord::Base
+        validates_presence_of :title, :description, :image_url
+        validates_length_of :title, :minimum =>10, :too_short => "should be longer than 10 characters"
+        validates_numericality_of :price, :message => "should be a number"
+        validates_uniqueness_of :title
+        validates_format_of :image_url,
+                :with => %r{\.(gif|jpg|png)$}i,
+                :message => 'must be a URL for GIF, JPG '+
+                           'or PNG image.'
+        validate :price_must_be_at_least_a_cent
+
+        protected
+        def price_must_be_at_least_a_cent
+                errors.add(:price, 'should be at least 0.01') if price.nil? || price< 0.01
+        end
 end
