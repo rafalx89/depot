ceea334d7c1928d040e30c9b1976561ea01506a8
Rafal <rafalx89@gmail.com>
2010-12-25 22:50:22 +0100

    add_to_cart in sidebar

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,40 @@
+class StoreController < ApplicationController
+  def index
+        @products = Product.find_products_for_sale
+        @counter = increment_count
+        @cart = find_cart
+       # @t=Time.now
+       # @time = @t.strftime("%Y-%m-%d %H:%M:%S")
+  end
+  def add_to_cart
+          product = Product.find(params[:id])
+          @cart = find_cart
+          @cart.add_product(product)
+          session[:count]=0
+          redirect_to_index
+  rescue ActiveRecord::RecordNotFound
+          logger.error("Attempt to access i nvalid product #{params[:id]}")
+          redirect_to_index("Invalid product")
+  end
+
+  def empty_cart
+          session[:cart]=nil
+          flash[:notice]= "Your cart is currently empty"
+          redirect_to_index("Your cart is currentyl empty")
+  end
+
+private 
+  def find_cart
+          session[:cart] ||=Cart.new
+  end
+
+  def redirect_to_index(msg = nil)
+          flash[:notice] = msg if msg
+          redirect_to :action => 'index'
+  end
+  def increment_count
+          session[:count] ||=0
+          session[:count] +=1
+  end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '3b34ed2a560888daf79d877696690bcdfcd6fc0cdb5db52115cf3ed4eacb0f2a2a1b9db6f51c2ba627716ace40820def26c3273d185b470bcd43d440de725c6e'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+ActionController::Base.session_store = :active_record_store
